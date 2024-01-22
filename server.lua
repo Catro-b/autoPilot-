66ca479069c43825658ec455384df62498068d65
CatRo-b satoshi Git <137897299+Catro-b@users.noreply.github.com>
2024-01-22 08:57:31 +0700

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,13 @@
+ESX = nil
+
+local resoureceName  =  GetCurrentResourceName()
+local script_Name = 'AutoPilot'
+TriggerEvent ('esx:getSharedObject' ,fumction(obj) ESX = obj end )
+
+RegisterCommand("go", function(source,args,raw
+  locol src = source
+  TriggerClientEvent("autopilot:start",src) end)
+
+if GetCurrentResourceName() = script_name then
+  print('^5 ['..script_name..']^2 Detials : ^5 Success !^2')
+end
